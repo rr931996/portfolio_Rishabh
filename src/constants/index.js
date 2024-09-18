5366eb5563a37a70c8ea4c1f55f060ed4d3d77cd
Rishabh.r <Rishabh.r@INVENTRAXH14>
2024-09-18 13:34:28 +0530

    npm update

--- src/constants/index.js
+++ src/constants/index.js
@@ -233,7 +233,7 @@ const projects = [
         color: "yellow-text-gradient",
       },
     ],
-    image: jobit,
+    image: tripguide,
     source_code_link: "https://myoms.nilkamal.online/",
   },
   {
